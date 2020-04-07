f0e8aef42bf418fd3ef9af3ea8a35689d83f0933
Luiz Felipe M. Pereira <luiz_mst4@hotmail.com>
2020-04-07 09:12:09 -0300

    test: REST testado

---
+++ src/main/java/com/luizfelipe/cursomc/resouces/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.luizfelipe.cursomc.resouces;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST funcionando!";
+	}
+}
